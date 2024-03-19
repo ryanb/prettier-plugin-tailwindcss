6daa38974dbeac34b5cda82a9f0979ec09bbe9eb
Ryan Bates <ryan@railscasts.com>
2024-03-19 11:02:20 -0700

    Support single quotes in ERB

--- tests/format.test.ts
+++ tests/format.test.ts
@@ -212,6 +212,7 @@ let glimmer: TestEntry[] = [
 const htmlErb: TestEntry[] = [
   ['<div class="sm:p-0 p-0"></div>', '<div class="p-0 sm:p-0"></div>'],
   ['<%= tag class: "sm:p-0 p-0" %>', '<%= tag class: "p-0 sm:p-0" %>'],
+  ["<%= tag class: 'sm:p-0 p-0' %>", "<%= tag class: 'p-0 sm:p-0' %>"],
   [
     '<div class="sm:p-0 p-0 <%= "m-2" %>"></div>',
     '<div class="p-0 sm:p-0 <%= "m-2" %>"></div>',
